5c88d51b183a89e85fd26c88a2e7f1697da799c3
Alexandr <smolnikov.456@mail.ru>
2023-12-10 16:18:51 +0500

    new packages added. bcrypt was removed

--- apps/second-app/src/main.ts
+++ apps/second-app/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { SecondAppModule } from './second-app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(SecondAppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+  console.log('second app started on port 3000');
 }
 bootstrap();
