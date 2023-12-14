3e8f7eec932f371a5967c532f5e624b8b844e0dd
Alexandr <smolnikov.456@mail.ru>
2023-12-14 10:28:55 +0500

    app config minor changes

--- libs/config/src/config.service.ts
+++ libs/config/src/config.service.ts
@@ -1,8 +1,8 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('APP_CONFIG', () => ({
-  FIRST_APP_PORT: 3021 || process.env.PORT,
-  SECOND_APP_PORT: 3022 || process.env.PORT,
+  FIRST_APP_PORT: parseInt(process.env.PORT) || 3021,
+  SECOND_APP_PORT: parseInt(process.env.PORT) || 3022,
   EMAIL_USER: process.env.EMAIL_USER,
   EMAIL_PASS: process.env.EMAIL_PASS,
 }));
