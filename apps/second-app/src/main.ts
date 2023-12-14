01583d26d6f64df5f1a1d5d6f60bb898e17f3e1e
Alexandr <smolnikov.456@mail.ru>
2023-12-15 01:09:17 +0500

    Add new packages (#2)
    * new packages added. bcrypt was removed
    
    * bcrypt added. nodemailer nestjs wrapper was removed
    
    * initial config
    
    * feat: create lib config
    
    ---------
    
    Co-authored-by: Artem Sankov <artemsankov0000@gmail.com>

--- apps/second-app/src/main.ts
+++ apps/second-app/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { SecondAppModule } from './second-app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(SecondAppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('APP_CONFIG.SECOND_APP_PORT');
+
+  await app.listen(port);
+
+  console.log(`second app started on port ${port}`);
 }
 bootstrap();
