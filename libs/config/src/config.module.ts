0242e695868c90097571977f87209600444c36a3
Alexandr <smolnikov.456@mail.ru>
2023-12-25 12:56:20 +0500

    fix: remove libs folder

--- libs/config/src/config.module.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigModule } from '@nestjs/config';
-import appConfig from './config.service';
-
-export const EnvModule = ConfigModule.forRoot({
-  isGlobal: true,
-  load: [appConfig],
-});
