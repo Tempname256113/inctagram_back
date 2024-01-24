741aed85b39b642a4dec2bc6eda59dae4f05a0f7
Alexandr <smolnikov.456@mail.ru>
2024-01-24 23:50:01 +0500

    feature: created new way to auth google instead using passport

---
+++ apps/first-app/src/auth/dto/response/sideAuth.responseType.ts
@@ -0,0 +1,5 @@
+export type SideAuthResponseType = {
+  userId: number;
+  username: string;
+  accessToken: string;
+};
--- shared/config/auth.config.service.ts
+++ shared/config/auth.config.service.ts
@@ -5,4 +5,5 @@ export default registerAs('AUTH_CONFIG', () => ({
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+  FRONTEND_URL: process.env.FRONTEND_URL,
 }));
