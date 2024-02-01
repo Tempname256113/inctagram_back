b49baa600068d6ae5f3b568cc3af0dd79dc3a8b5
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2024-02-01 12:29:06 +0300

    add first commit

--- shared/database/prisma.service.ts
+++ shared/database/prisma.service.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { Injectable, OnModuleInit } from '@nestjs/common';
-
+/*  */
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
