cc3246161063f0d91f4d86a57249ff333a4f6d2b
Alexandr <smolnikov.456@mail.ru>
2024-02-09 14:27:54 +0500

    deploy

--- apps/first-app/src/auth/auth.controller.ts
+++ apps/first-app/src/auth/auth.controller.ts
@@ -55,7 +55,7 @@ import { refreshTokenCookieTitle } from './variables/refreshTokenTitle';
 @ApiTags('auth controllers')
 export class AuthController {
   constructor(private readonly commandBus: CommandBus) {}
-  //
+  //deploy
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
   @RegisterRouteSwaggerDescription()
