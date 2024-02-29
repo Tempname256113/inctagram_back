5ff52143d58622cf7394fbf4672b871cfd184047
Alexandr <smolnikov.456@mail.ru>
2024-02-29 12:35:51 +0500

    remove prisma addon

--- shared/database/prisma.service.ts
+++ shared/database/prisma.service.ts
@@ -1,31 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import kyselyExtension from 'prisma-extension-kysely';
-import { DB } from '../../prisma/generated/types';
-import {
-  Kysely,
-  PostgresAdapter,
-  PostgresIntrospector,
-  PostgresQueryCompiler,
-} from 'kysely';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  prismaWithAddon = new PrismaClient().$extends(
-    kyselyExtension({
-      kysely: (driver) =>
-        new Kysely<DB>({
-          dialect: {
-            createDriver: () => driver,
-            createAdapter: () => new PostgresAdapter(),
-            createIntrospector: (db) => new PostgresIntrospector(db),
-            createQueryCompiler: () => new PostgresQueryCompiler(),
-          },
-        }),
-    }),
-  );
-
   async onModuleInit() {
-    await Promise.all([this.prismaWithAddon.$connect(), this.$connect()]);
+    await this.$connect();
   }
 }
